2f419b895724edda2461c0f655f7e3da68a35a23
jchiu <jessiechiu20@gmail.com>
2025-03-13 14:07:59 +0800

    track attractions_db and update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from fastapi import *
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
+import mysql.connector, json
+from typing import Optional
+
+dbconfig = {
+    "user":"root",
+    "password":"1234",
+    "host":"localhost",
+    "database":"tpe_trip"
+    }
+cnx = mysql.connector.connect(pool_name = "pool", pool_size = 10, **dbconfig)
+
+
 app=FastAPI()
+
 
 # Static Pages (Never Modify Code in this Block)
 @app.get("/", include_in_schema=False)
@@ -15,3 +28,183 @@ async def booking(request: Request):
 @app.get("/thankyou", include_in_schema=False)
 async def thankyou(request: Request):
 	return FileResponse("./static/thankyou.html", media_type="text/html")
+
+
+
+@app.get("/api/attractions")
+async def get_attractions(page: int, keyword: Optional[str] = None):
+
+	try:
+
+		cnx = mysql.connector.connect(pool_name = "pool")
+		cursor = cnx.cursor(dictionary=True)
+
+		if keyword is None:
+
+			cursor.execute("SELECT * FROM attractions")
+			attraction_list = cursor.fetchall()
+
+			page_start_index = page*12
+			page_end_index = (page+1)*12
+
+			if page_start_index > len(attraction_list):
+				
+				cnx.close()
+				return{
+					"message":"No Data. Data index out of range."
+				}
+		
+			elif page_end_index <= len(attraction_list):
+
+				attractions_data = []
+
+				for i in range(page_start_index, page_end_index):
+					attraction_list[i]["images"] = json.loads(attraction_list[i]["images"])
+					attractions_data.append(attraction_list[i])
+				
+				
+				
+				cnx.close()	
+				return{
+					"nextPage": page + 1,
+					"data": attractions_data
+				}
+			
+			elif len(attraction_list) - page_start_index < 12:
+
+				attractions_data = []
+
+				for i in range(page_start_index, len(attraction_list)):
+					attraction_list[i]["images"] = json.loads(attraction_list[i]["images"])
+					attractions_data.append(attraction_list[i])
+
+				cnx.close()				
+				return{
+					"nextPage": None,
+					"data": attractions_data
+				}
+
+
+		if keyword is not None:
+
+			cursor.execute("SELECT * FROM attractions WHERE mrt = %s OR name LIKE %s", (keyword, f"%{keyword}%"))
+			attraction_list = cursor.fetchall()
+
+			page_start_index = page*12
+			page_end_index = (page+1)*12
+
+			if len(attraction_list) == 0:
+
+				cnx.close()
+				return{
+					"message":"Can't find any matched data."
+				}
+					
+			elif page_end_index <= len(attraction_list):
+
+				attractions_data = []
+
+				for i in range(page_start_index, page_end_index):
+					attraction_list[i]["images"] = json.loads(attraction_list[i]["images"])
+					attractions_data.append(attraction_list[i])
+
+				cnx.close()
+				return{
+					"nextPage": page + 1,
+					"data": attractions_data
+				}
+			
+			elif 0 < len(attraction_list) - page_start_index < 12:
+
+				attractions_data = []
+
+				for i in range(page_start_index, len(attraction_list)):
+					attraction_list[i]["images"] = json.loads(attraction_list[i]["images"])
+					attractions_data.append(attraction_list[i])
+
+				cnx.close()
+				return{
+					"nextPage": None,
+					"data": attractions_data
+				}
+
+			else:
+				return{
+					"message": "No Data. Data index out of range."
+				}
+	
+	except:
+		cnx.close()
+		return JSONResponse(
+        status_code =500,
+        content = {
+			"error": True,
+			"message": "Internal Server Error"
+		})
+
+
+
+
+@app.get("/api/attraction/{attractionId}")
+async def get_attraction_by_id(attractionId:int):
+
+	try:
+
+		cnx = mysql.connector.connect(pool_name = "pool")
+		cursor = cnx.cursor(dictionary=True)
+		cursor.execute("SELECT * FROM attractions WHERE id = %s", (attractionId,))
+		attraction_data = cursor.fetchone()
+		attraction_data["images"] = json.loads(attraction_data["images"])
+
+		if attraction_data == None:
+			cnx.close()
+			return JSONResponse(
+        	status_code = 400,
+        	content = {
+			"error": True,
+			"message": "Id is out of range."
+		})
+
+		cnx.close()
+		return{
+			"data": attraction_data
+		}
+	
+	
+	except:
+		cnx.close()
+		return JSONResponse(
+        status_code =500,
+        content = {
+			"error": True,
+			"message": "Internal Server Error"
+		})
+	
+
+@app.get("/api/mrts")
+async def get_mrts_desc():
+	try:
+		cnx = mysql.connector.connect(pool_name = "pool")
+		cursor = cnx.cursor()
+		cursor.execute("SELECT mrt FROM attractions GROUP BY mrt ORDER BY COUNT(mrt) DESC")
+		mrts = cursor.fetchall()
+		mrts.pop()
+		
+		mrt_list = []
+		for mrt in mrts:
+			mrt_list.append(mrt[0])
+		
+		cnx.close()
+
+		return{
+			"data":mrt_list
+		}
+	
+	except:
+		cnx.close()
+		return JSONResponse(
+        status_code =500,
+        content = {
+			"error": True,
+			"message": "Internal Server Error"
+		})
---
+++ attractions_db.py
@@ -0,0 +1,51 @@
+import json
+import mysql.connector
+
+dbconfig = {
+    "user":"root",
+    "password":"1234",
+    "host":"localhost",
+    "database":"tpe_trip"
+    }
+
+def read_json_file():
+    with open("data/taipei-attractions.json") as json_file:
+        json_data = json.load(json_file)
+        spots_data= json_data["result"]["results"]
+        return spots_data
+    
+def filter_imgs(spot_img):
+    file_string = spot_img
+    split_url = file_string.split("https://")
+    split_url = [p for p in split_url if p] #清除空item
+    img_url = ["https://" + p for p in split_url if p.lower().endswith("jpg") or p.lower().endswith("png")]
+    return img_url
+
+
+#INSERT into DB
+spots = read_json_file()
+cnx = mysql.connector.connect(**dbconfig)
+cursor = cnx.cursor()
+# print('資料庫連線成功')
+for spot in spots:
+    name = spot["name"]
+    category = spot["CAT"]
+    description = spot["description"]
+    address = spot["address"]
+    transport = spot["direction"]
+    mrt = spot["MRT"]
+    latitude = spot["latitude"]
+    longitude = spot["longitude"]
+    images = json.dumps(filter_imgs(spot["file"]))
+
+    cursor.execute("""
+    INSERT INTO attractions(name, category, description, address, transport, mrt, lat, lng, images)
+                    VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+                    (name, category, description, address, transport, mrt, latitude,longitude, images))
+    cnx.commit()
+
+cnx.close()
+    
+
+
+
