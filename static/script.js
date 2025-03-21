9c7e051445bf022b587f026751061e0b06c9673e
jchiu <jessiechiu20@gmail.com>
2025-03-21 15:11:43 +0800

    update tasks 2-4 & 2-5

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 //fetch mrt list
 function fetchMRTList(){
-    fetch('/api/mrts')
+    return fetch('/api/mrts')
     .then(response => response.json())
     .then(data => {
         mrtData = data["data"] 
@@ -8,11 +8,9 @@ function fetchMRTList(){
             createMRTContainer().textContent = mrtName  
         });
         })
-    .catch(error => {
-        console.error('發生錯誤:', error);
-        });
 }
-    
+
+//create mrt list container
 function createMRTContainer(){
     let mrtList = document.getElementById('mrt-list')
     let mrtItem = document.createElement("li")
@@ -21,13 +19,27 @@ function createMRTContainer(){
     return mrtItem
 }
 
-fetchMRTList();
+//search attractions by mrt
+fetchMRTList().then(() => {
+    let mrt_list_item = document.querySelectorAll(".mrt-list-item")
+    console.log(mrt_list_item)
+    mrt_list_item.forEach(itemElement => {
+        itemElement.addEventListener('click', function(){
+            let mrtName = itemElement.textContent
+            let input = document.getElementById('slogan-input')
+            input.value = mrtName
+            // 建立 submit 事件，觸發 submit 事件
+            let submitEvent = new Event('submit')
+            let sloganForm = document.getElementById('slogan-form')
+            sloganForm.dispatchEvent(submitEvent)
+        })       
+    });
+})
 
 
-//fetch attractions without keyword
-let page = 0
-function loadAttractions(){
-    return fetch(`/api/attractions?page=${page}`)
+//render attractions  
+function loadAttractions(page = 0, keyword = ''){
+    return fetch(`/api/attractions?page=${page}&keyword=${keyword}`)
     .then(response => response.json())
     .then(data => {
         console.log(data);
@@ -43,32 +55,68 @@ function loadAttractions(){
     })
 }
 
-//initial load //debug為何第一頁會 load 兩次 //不要把 page 設為全局變數 //部署上線測試
+let globalObserver = null
+
+//render attractions without keyword
 loadAttractions().then(nextPage => {
         page = nextPage
-        let observer = new IntersectionObserver((entries) => {
-            //load more...
-             if(entries[0].isIntersecting)
-             loadAttractions().then(nextPageValue => {
-                if(nextPageValue !== null){
-                    page = nextPageValue;
-                }
-                else{
-                    observer.disconnect();
-                }
-            });
-            }
-         );
-         let target = document.getElementById("footer")
-         observer.observe(target);
-    
+        globalObserver = new IntersectionObserver((entries) => {
+
+                if(entries[0].isIntersecting)
+                loadAttractions(page).then(nextPageValue => {
+                    if(nextPageValue !== null){
+                        page = nextPageValue;
+                    }
+                    else{
+                        globalObserver.disconnect();
+                    }
+                });
+        });
+        let target = document.getElementById("footer")
+        globalObserver.observe(target);
 });
 
 
+//render attractions with keyword
+let form = document.getElementById("slogan-form");
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    if(globalObserver){
+        globalObserver.disconnect();
+    }
+
+    let inputValue = document.getElementById("slogan-input").value
+    if(inputValue !== "輸入景點名稱查詢"){
+
+    const attractionsSection = document.getElementById("attractions-section");
+    attractionsSection.replaceChildren();
+
+    loadAttractions(0, inputValue).then(nextPage => {
+        if(nextPage != null){
+            page = nextPage
+            globalObserver = new IntersectionObserver((entries) => {
+                //load more...
+                    if(entries[0].isIntersecting)
+                    loadAttractions(page, inputValue).then(nextPageValue => {
+                        if(nextPageValue !== null){
+                            page = nextPageValue;
+                        }
+                        else{
+                            globalObserver.disconnect();
+                        }
+                    });
+            });
+            let target = document.getElementById("footer")
+            globalObserver.observe(target);
+        }
+    });
+}   
+})
 
 
 function createAttractionContainer(){
-
     let attractionsSection = document.getElementById('attractions-section')
     let attractionCard = document.createElement("div")
     let attractionInfo = document.createElement("div")
@@ -102,57 +150,58 @@ function createAttractionContainer(){
 
 
 
+let mrtList = document.getElementById('mrt-list');
+let leftBtn = document.querySelector('.list-bar-leftBtn');
+let rightBtn = document.querySelector('.list-bar-rightBtn');
+let leftBtn_icon = document.getElementById('leftBtn-icon');
+let rightBtn_icon = document.getElementById('rightBtn-icon');
 
+// mrt_list 捲動
+document.addEventListener('DOMContentLoaded', function() {
+    let scrollAmount = 200;
 
-
-
-
-    let mrtList = document.getElementById('mrt-list');
-    let leftBtn = document.querySelector('.list-bar-leftBtn');
-    let rightBtn = document.querySelector('.list-bar-rightBtn');
-    let leftBtn_icon = document.getElementById('leftBtn-icon');
-    let rightBtn_icon = document.getElementById('rightBtn-icon');
-    let slogan_input = document.querySelector('#slogan-form input');
-
-    // mrt_list 捲動
-    document.addEventListener('DOMContentLoaded', function() {
-        let scrollAmount = 200;
-
-        leftBtn.addEventListener('click', function() {
-            mrtList.scrollBy({
-                left: -scrollAmount,
-                behavior: 'smooth'
-            });
-        });
-        
-        rightBtn.addEventListener('click', function() {
-            mrtList.scrollBy({
-                left: scrollAmount,
-                behavior: 'smooth'
-            });
-
+    leftBtn.addEventListener('click', function() {
+        mrtList.scrollBy({
+            left: -scrollAmount,
+            behavior: 'smooth'
         });
     });
-
-    //mrt-list icon hover
-    leftBtn.addEventListener('mouseover', function(){
-            leftBtn_icon.src = "/static/images/left-hovered.png"
-        });
-    leftBtn.addEventListener('mouseout', function(){
-            leftBtn_icon.src = "/static/images/left-default.png"
-        });
-    rightBtn.addEventListener('mouseover', function(){
-            rightBtn_icon.src = "/static/images/right-hovered.png"
-        });
-    rightBtn.addEventListener('mouseout', function(){
-            rightBtn_icon.src = "/static/images/right-default.png"
+    
+    rightBtn.addEventListener('click', function() {
+        mrtList.scrollBy({
+            left: scrollAmount,
+            behavior: 'smooth'
         });
 
-    //input default value
-    slogan_input.addEventListener('focus', function(){
-      slogan_input.value = ''
-    })
+    });
+});
 
-    slogan_input.addEventListener('blur', function(){
-      slogan_input.value = '輸入景點名稱查詢'
-    })
+//mrt-list icon hover
+leftBtn.addEventListener('mouseover', function(){
+        leftBtn_icon.src = "/static/images/left-hovered.png"
+    });
+leftBtn.addEventListener('mouseout', function(){
+        leftBtn_icon.src = "/static/images/left-default.png"
+    });
+rightBtn.addEventListener('mouseover', function(){
+        rightBtn_icon.src = "/static/images/right-hovered.png"
+    });
+rightBtn.addEventListener('mouseout', function(){
+        rightBtn_icon.src = "/static/images/right-default.png"
+    });
+
+
+
+//input default value
+let slogan_input = document.getElementById('slogan-input');
+slogan_input.addEventListener('focus', function(){
+    if (slogan_input.value == '輸入景點名稱查詢') {
+        slogan_input.value = '';
+    }
+});
+
+slogan_input.addEventListener('blur', function(){
+    if (slogan_input.value == '') {
+        slogan_input.value = '輸入景點名稱查詢';
+    }
+});
